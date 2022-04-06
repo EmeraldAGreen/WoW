4ac3c279b8f66d0987c1b42c3600539a89f16e17
David Warner <david.hans.warner@gmail.com>
2022-04-05 20:56:02 -0600

    added functional login/signup buttons in view  and added code to homeRoutes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -2,6 +2,36 @@ const router = require('express').Router();
 const { Workout } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// GET all locations
+router.get('/', async (req, res) => {
+  try {
+    const workoutData = await Workout.findAll();
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single location
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const workoutData = await Workout.findByPk(req.params.id, {
+//       // JOIN with travellers, using the Trip through table
+//       include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+//     });
+
+//     if (!locationData) {
+//       res.status(404).json({ message: 'No location found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(locationData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newWorkout = await Workout.create({
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,28 +4,27 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all workouts and JOIN with user data
-    const workoutData = await workout.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    //   // Get all workouts and JOIN with user data
+    //   const workoutData = await workout.findAll({
+    //     include: [
+    //       {
+    //         model: User,
+    //         attributes: ['name'],
+    //       },
+    //     ],
+    //   });
 
-    // Serialize data so the template can read it
-    const workouts = workoutData.map((workout) => workout.get({ plain: true }));
+    //   // Serialize data so the template can read it
+    //   const workouts = workoutData.map((workout) => workout.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      workouts, 
-      logged_in: req.session.logged_in 
-    });
+    res.status(200).render('home');
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
 
 router.get('/workout/:id', async (req, res) => {
   try {
@@ -77,6 +76,20 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/home', (req, res) => {
+  res.render('home');
 });
 
 module.exports = router;
