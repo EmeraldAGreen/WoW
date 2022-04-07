3337bd7ef8d0f1d3e9c26215ecf3672c9ab9f2f5
Emerge <egreengirl1@gmail.com>
2022-04-06 20:00:32 -0600

    comments frontend

---
+++ public/js/comment.js
@@ -0,0 +1,31 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+  
+    const comment = document
+      .querySelector('#comment')
+      .value.trim();
+  
+    if (comment) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          blog_id,
+          comment_text,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+        document.querySelector('#comment-form').style.display = 'block';
+      }
+    }
+  }
+  
+  document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
