b573de7713d9447dd45dbbff497387c2efbb5fa4
David Warner <david.hans.warner@gmail.com>
2022-04-09 13:15:16 -0600

    added dashboard route functionality and cleaned up misc items

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Workout, Comment, Tag, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// render (get) all workout w/ comments (not user specific)
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all workouts and JOIN with user data
+        const workoutData = await Workout.findAll({
+            attributes: ['id', 'name', 'description', 'duration', 'tag_id', 'user_id', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment',
+                        'workout_id',
+                        'user_id',
+                        'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        console.log(workoutData);
+        // Serialize data so the template can read it
+        const workouts = workoutData.map((allWorkouts) => allWorkouts.get({ plain: true }));
+        console.log(workouts);
+        // Pass serialized data and session flag into template
+        res.render('dashboard', {
+            workouts,
+            logged_in: req.session.logged_in,
+            username: req.session.username,
+        });
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,66 +2,32 @@ const router = require('express').Router();
 const { Workout, Comment, Tag, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+// /home
 router.get('/', async (req, res) => {
-  console.log('we made it! ')
-  try {
-      // Get all workouts and JOIN with user data
-      const workoutData = await Workout.findAll({
-        attributes: ['id', 'name', 'description', 'duration', 'tag_id', 'user_id', 'created_at'],
-        include: [
-          {
-            model: Comment,
-            attributes: [
-                'id', 
-                'comment', 
-                'workout_id', 
-                'user_id', 
-                'created_at'],
-            include: {
-              model: User,
-              attributes: ['name'],
-          },
-        },
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
-      console.log(workoutData);
-      // Serialize data so the template can read it
-      const workouts = workoutData.map((allWorkouts) => allWorkouts.get({ plain: true }));
-console.log(workouts);
-    // Pass serialized data and session flag into template
-    res.render('home', {
-      workouts, 
-      logged_in: req.session.logged_in, 
-      username: req.session.username,
-    });
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
+  // Pass serialized data and session flag into template
+  res.render('home');
 });
 
-router.get('/dashboard', (req, res) => {
+// /dashboard
+router.get('/', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/my');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
 });
 
-// router.get('/workouts', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/');
-//     return;
-//   }
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/my-workouts');
+    return;
+  }
 
-//   res.render('login');
-// });
+  res.render('login');
+});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
